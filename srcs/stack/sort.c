eb9ff90daa4f3259e3665d6ea0a6782c8ec02b3f
dev-carbon <humanfou@student.42.fr>
2021-12-03 20:46:28 +0100

    sorting average reduce to 600 for 100 values and 8000 for 500 values

--- srcs/stack/sort.c
+++ srcs/stack/sort.c
@@ -169,7 +169,7 @@ void	print_list(t_stack *stack)
 	}
 }
 
-t_item	get_min_sibling(t_stack *s, int val)
+t_item	get_min_bottom_sibbling(t_stack *s, int val)
 {
 	t_item			min;
 	t_stack_list	*l;
@@ -179,17 +179,13 @@ t_item	get_min_sibling(t_stack *s, int val)
 	while (l)
 	{
 		if (l->item.value < min.value && l->item.value > val)
-		{
 			min = l->item;
-			// break ;
-		}
 		l = l->next;
 	}
-	// printf("min sibbling: %d\n", min.value);
 	return(min);
 }
 
-t_item	get_max_sibling(t_stack *s, int val)
+t_item	get_max_bottom_sibling(t_stack *s, int val)
 {
 	int				n;
 	t_item			max;
@@ -208,11 +204,44 @@ t_item	get_max_sibling(t_stack *s, int val)
 	return(max);
 }
 
-t_stack	*sort_md(t_stack *a, t_stack *b, int *hits)
+void	create_partition(t_stack *a, t_stack *b, int *hits)
+{
+	int		pivot;
+
+	pivot = get_pivot(a, b);
+	while (1)
+	{
+		if (is_sorted(a, ASC))
+			break ;
+		if (a->top.value < pivot)
+			*hits += push_stack(b, a, "pb\n");
+		else
+			*hits += rotate_stack(a, "ra\n");
+		pivot = get_pivot(a, b);
+	}
+}
+
+int	is_rotate_min(t_stack *stack)
 {
 	int	min_gap;
 	int	max_gap;
-	t_item	near;
+
+	min_gap = stack->min.index;
+	max_gap = stack->max.index;
+	if (stack->min.index >= stack->size / 2)
+		min_gap = stack->size - (stack->min.index);
+	if (stack->max.index >= stack->size / 2)
+		max_gap = stack->size - (stack->max.index);
+	if (min_gap < max_gap)
+		return (1);
+	return (0);
+}
+
+t_stack	*sort_md(t_stack *a, t_stack *b, int *hits)
+{
+
+	t_item	bottom_sibbling;
+
 	int	pivot;
 	int count_rra = 0;
 	int count_ra = 0;
@@ -224,335 +253,343 @@ t_stack	*sort_md(t_stack *a, t_stack *b, int *hits)
 	int	count_rr = 0;
 
 	printf("********* PARTITION *******\n");
-	while (1)
-	{
-		if (is_sorted(a, ASC))
-			break ;
-		if (a->top.value > a->last.value)
-			*hits += rotate_stack(a, "ra\n");
-		else
-		{
-			*hits += push_stack(b, a, "pb\n");
-			count_pb++;
-		}
-	}
+	create_partition(a, b, hits);
 	printf("********* END PARTITION *******\n");
 	display_stacks(a, b);
 	// exit(0);
 	while (1)
 	{
-		// break ;
 		if (is_empty(b))
-			break ;
-
-		if (b->min.index < b->size / 2)
-			min_gap = b->min.index;
-		else
-			min_gap = b->size - (b->min.index);
-		if (b->max.index < b->size / 2)
-			max_gap = b->max.index;
-		else
-			max_gap = b->size - (b->max.index);
-		// printf("min| gap: %d, [%d] => %d\n", min_gap, b->min.index, b->min.value);
-		// printf("max| gap: %d, [%d] => %d\n", max_gap, b->max.index, b->max.value);
+			break ;				
 		
-		// move the minimun of B at the correct position in A
-		if (min_gap < max_gap)
+		if (is_rotate_min(b))
 		{
-			near = get_min_sibling(a, b->min.value);
-			printf("move min %d above %d\n", b->min.value, near.value);
+			bottom_sibbling = get_min_bottom_sibbling(a, b->min.value);
+			printf("move min %d above %d\n", b->min.value, bottom_sibbling.value);
 			
-			// preform rrr
 			if (b->min.index > (b->size / 2))
 			{
-				// preform rra ?
-				if (near.index > (a->size / 2))
+				if (bottom_sibbling.index > (a->size / 2))
 				{
-					// printf("performing min rrr\n");
-					while (b->min.index != 0 && near.value != a->top.value)
+					while (b->min.index != 0 && bottom_sibbling.value != a->top.value)
 					{
-						*hits += rev_rotate_stacks(a, b, "rrr\n");
-						display_stacks(a, b);
-						count_rrr++;
+						if (a->top.value > b->top.value && b->top.value > a->last.value)
+						{
+							*hits += push_stack(a, b, "pa => min => rrr\n");
+							display_stacks(a, b);
+						}
+						else if (a->top.value > b->top.value && is_sorted(a, ASC))
+						{
+							*hits += push_stack(a, b, "pa => min => rrr 2\n");
+							display_stacks(a, b);
+						}
+						else
+						{
+							*hits += rev_rotate_stacks(a, b, "rrr\n");
+							display_stacks(a, b);
+						}
 					}
 
-					// do the rest of rra if possible
-					while (near.value != a->top.value)
+					while (bottom_sibbling.value != a->top.value)
 					{
-						// printf("performing min rra\n");
-						*hits += rev_rotate_stack(a, "rra\n");
-						display_stacks(a, b);
-						count_rra++;
+						if (a->top.value > b->top.value && b->top.value > a->last.value)
+						{
+							*hits += push_stack(a, b, "pa => min => rra\n");
+							display_stacks(a, b);
+						}
+						else if (a->top.value > b->top.value && is_sorted(a, ASC))
+						{
+							*hits += push_stack(a, b, "pa => min => rrr 2\n");
+							display_stacks(a, b);
+						}
+						else
+						{
+							*hits += rev_rotate_stack(a, "rra\n");
+							display_stacks(a, b);
+						}
 					}
 				}
 				else
 				{
-					// printf("performing min ra\n");
-					// perform ra
-					while (near.value != a->top.value)
+					while (bottom_sibbling.value != a->top.value)
 					{
-						// printf("ok\n");
-						*hits += rotate_stack(a, "ra\n");
-						if (a->top.value > b->top.value && b->top.value > b->min.value)
+						if (a->top.value > b->top.value && b->top.value > a->last.value)
 						{
-							*hits += push_stack(a, b, "pa\n");
-							break ;
+							*hits += push_stack(a, b, "pa => min => ra\n");
+							display_stacks(a, b);
 						}
-						display_stacks(a, b);
-						count_ra++;
+						else if (a->top.value > b->top.value && is_sorted(a, ASC))
+						{
+							*hits += push_stack(a, b, "pa => min => rrr 2\n");
+							display_stacks(a, b);
+						}
+						else
+						{
+							*hits += rotate_stack(a, "ra\n");
+							display_stacks(a, b);
+						}
 					}
 				}
 
-				// do the rest of rrb if possible
 				while (b->min.index != 0)
 				{
-					// printf("performing min rrb\n");
-					*hits += rev_rotate_stack(b, "rrb\n");
-					display_stacks(a, b);
-					count_rrb++;
+					if (a->top.value > b->top.value && b->top.value > a->last.value)
+					{
+						*hits += push_stack(a, b, "pa => min => rrb\n");
+						display_stacks(a, b);
+					}
+					else if (a->top.value > b->top.value && is_sorted(a, ASC))
+					{
+						*hits += push_stack(a, b, "pa => min => rrr 2\n");
+						display_stacks(a, b);
+					}
+					else
+					{
+						*hits += rev_rotate_stack(b, "rrb\n");
+						display_stacks(a, b);
+					}
 				}
 			}
-			// perform rr
 			else
 			{
-				if (near.index <= (a->size / 2))
+				if (bottom_sibbling.index <= (a->size / 2))
 				{
-					while (b->min.index != 0 && near.value != a->top.value)
+					while (b->min.index != 0 && bottom_sibbling.value != a->top.value)
 					{
-						// printf("ok\n");
-						*hits += rotate_stacks(a, b, "rr\n");
-						display_stacks(a, b);
-						count_rr++;
+						if (a->top.value > b->top.value && b->top.value > a->last.value)
+						{
+							*hits += push_stack(a, b, "pa => min => rr");
+							display_stacks(a, b);
+						}
+						else
+						{
+							*hits += rotate_stacks(a, b, "rr\n");
+							display_stacks(a, b);
+						}
 					}
-					// perform the rest of ra
-					while (near.value != a->top.value)
+
+					while (bottom_sibbling.value != a->top.value)
 					{
-						// printf("ok\n");
-						*hits += rotate_stack(a, "ra\n");
-						display_stacks(a, b);
-						count_ra++;
+						if (a->top.value > b->top.value && b->top.value > a->last.value)
+						{
+							*hits += push_stack(a, b, "pa => min => ra");
+							display_stacks(a, b);
+						}
+						else
+						{
+							*hits += rotate_stack(a, "ra\n");
+							display_stacks(a, b);
+						}
 					}
-					
 				}
 				else
 				{
-					// perform rra
-					while (near.value != a->top.value)
+					while (bottom_sibbling.value != a->top.value)
 					{
-						*hits += rev_rotate_stack(a, "rra\n");
-						display_stacks(a, b);
-						count_rra++;
+						if (a->top.value > b->top.value && b->top.value > a->last.value)
+						{
+							*hits += push_stack(a, b, "pa => min => rra\n");
+							display_stacks(a, b);
+						}
+						else
+						{
+							*hits += rev_rotate_stack(a, "rra\n");
+							display_stacks(a, b);
+						}
 					}
+
 				}
 
 				while (b->min.index != 0)
 				{
-					// printf("performing min rb\n");
-					*hits += rotate_stack(b, "rb\n");
-					display_stacks(a, b);
-					count_rb++;
+					if (a->top.value > b->top.value && b->top.value > a->last.value)
+					{
+						*hits += push_stack(a, b, "pa => min => rb\n");
+						display_stacks(a, b);
+					}
+					else
+					{
+						*hits += rotate_stack(b, "rb\n");
+						display_stacks(a, b);
+					}
 				}
 			}
 		}
-		// move the maximum of B at the right position in A
 		else
 		{
-			// printf("min| gap: %d, [%d] => %d\n", min_gap, b->min.index, b->min.value);
-			// printf("max| gap: %d, [%d] => %d\n", max_gap, b->max.index, b->max.value);
-
-			near = get_max_sibling(a, b->max.value);
-			// printf("move max %d above %d\n", b->max.value, near.value);
-			
-			// preform rrr
+			bottom_sibbling = get_max_bottom_sibling(a, b->max.value);
+			printf("move max %d above %d\n", b->max.value, bottom_sibbling.value);
 			if (b->max.index > (b->size / 2))
 			{
-				// preform rra ?
-				if (near.index > (a->size / 2))
+				if (bottom_sibbling.index > (a->size / 2))
 				{
-					// printf("performinf max rrr\n");
-					while (b->max.index != 0 && near.value != a->top.value)
+					while (b->max.index != 0 && bottom_sibbling.value != a->top.value)
 					{
-						*hits += rev_rotate_stacks(a, b, "rrr\n");
-						display_stacks(a, b);
-						count_rrr++;
+						// to check
+						if (a->top.value > b->top.value && b->top.value > a->last.value)
+						{
+							*hits += push_stack(a, b, "pa => max => rrr\n");
+							display_stacks(a, b);
+						}
+						else
+						{
+							*hits += rev_rotate_stacks(a, b, "rrr\n");
+							display_stacks(a, b);
+						}
 					}
 
-					// do the rest of rra if possible
-					while (near.value != a->top.value)
+					while (bottom_sibbling.value != a->top.value)
 					{
-						// printf("performing max rra\n");
-						*hits += rev_rotate_stack(a, "rra\n");
-						display_stacks(a, b);
-						count_rra++;
+						// to check
+						if (a->top.value > b->top.value && b->top.value > a->last.value)
+						{
+							*hits += push_stack(a, b, "pa => max => rra\n");
+							display_stacks(a, b);
+						}
+						else
+						{
+							*hits += rev_rotate_stack(a, "rra\n");
+							display_stacks(a, b);
+						}
 					}
 				}
 				else
 				{
-					// perform ra
-					while (near.value != a->top.value)
+					while (bottom_sibbling.value != a->top.value)
 					{
-						*hits += rotate_stack(a, "ra\n");
-						display_stacks(a, b);
-						count_ra++;
+						// to check
+						if (a->top.value > b->top.value && b->top.value > a->last.value)
+						{
+							*hits += push_stack(a, b, "pa => max => ra\n");
+							display_stacks(a, b);
+						}
+						else
+						{
+							*hits += rotate_stack(a, "ra\n");
+							display_stacks(a, b);
+						}
 					}
 				}
 
-				// do the rest of rrb if possible
 				while (b->max.index != 0)
 				{
-					*hits += rev_rotate_stack(b, "rrb\n");
-					display_stacks(a, b);
-					count_rrb++;
+					// to check
+					if (a->top.value > b->top.value && b->top.value > a->last.value)
+					{
+						*hits += push_stack(a, b, "pa => max => rrb\n");
+						display_stacks(a, b);
+					}
+					else
+					{
+						*hits += rev_rotate_stack(b, "rrb\n");
+						display_stacks(a, b);
+					}
 				}
 			}
-			// perform rr
 			else
 			{
-				if (near.index <= (a->size / 2))
+				if (bottom_sibbling.index <= (a->size / 2))
 				{
-					while (b->max.index != 0 && near.value != a->top.value)
+					while (b->max.index != 0 && bottom_sibbling.value != a->top.value)
 					{
-						*hits += rotate_stacks(a, b, "rr\n");
-						display_stacks(a, b);
-						count_rr++;
+						if (a->top.value > b->top.value && b->top.value > a->last.value)
+						{
+							*hits += push_stack(a, b, "pa => max => rr\n");
+							display_stacks(a, b);
+						}
+						else
+						{
+							*hits += rotate_stacks(a, b, "rr\n");
+							display_stacks(a, b);
+						}
 					}
-					// perform the rest of ra
-					while (near.value != a->top.value)
+
+					while (bottom_sibbling.value != a->top.value)
 					{
-						*hits += rotate_stack(a, "ra\n");
-						display_stacks(a, b);
-						count_ra++;
+						// to check
+						if (a->top.value > b->top.value && b->top.value > a->last.value)
+						{
+							*hits += push_stack(a, b, "pa => max => ra\n");
+							display_stacks(a, b);
+						}
+						else
+						{
+							*hits += rotate_stack(a, "ra\n");
+							display_stacks(a, b);
+						}
 					}
 					
 				}
 				else
 				{
-					// perform rra
-					while (near.value != a->top.value)
+					while (bottom_sibbling.value != a->top.value)
 					{
-						*hits += rev_rotate_stack(a, "rra\n");
-						display_stacks(a, b);
-						count_rra++;
+						// to check
+						if (a->top.value > b->top.value && b->top.value > a->last.value)
+						{
+							*hits += push_stack(a, b, "pa => max => rra\n");
+							display_stacks(a, b);
+						}
+						else
+						{
+							*hits += rev_rotate_stack(a, "rra\n");
+							display_stacks(a, b);
+						}
 					}
 				}
 
-				// do the rest of rb if possible
 				while (b->max.index != 0)
 				{
-					*hits += rotate_stack(b, "rb\n");
-					display_stacks(a, b);
-					count_rb++;
+					// to check
+					if (a->top.value > b->top.value && b->top.value > a->last.value)
+					{
+						*hits += push_stack(a, b, "pa => max => rb\n");
+						display_stacks(a, b);
+					}
+					else
+					{
+						*hits += rotate_stack(b, "rb\n");
+						display_stacks(a, b);
+					}
 				}
+			}
+		}
+
+		if (bottom_sibbling.value == a->top.value)
+		{
+			if (b->min.index == 0 || b->max.index == 0)
+			{
+				*hits += push_stack(a, b, "pa\n");
+				display_stacks(a, b);
 			}
 		}
 	}
-	// printf("total hits: %d\n", *hits);
+
 	while (1)
 	{
 		if (is_sorted(a, ASC))
 			break ;
-		// break;
 		if (a->min.index > a->size / 2)
-		{
 			while (a->min.index != 0)
-			{
 				*hits += rev_rotate_stack(a, "rra\n");
-				display_stacks(a, b);
-				count_rra++;
-			}
-		}
 		else
-		{
 			while (a->min.index != 0)
-			{
 				*hits += rotate_stack(a, "ra\n");
-				display_stacks(a, b);
-				count_ra++;
-			}
-		}
 	}
 
-	// display_stacks(a, b);
+	display_stacks(a, b);
 
-	// printf("coun_pa : %d\n", count_pa);
-	// printf("coun_pb: %d\n", count_pb);
-	// printf("coun_ra: %d\n", count_ra);
-	// printf("coun_rra: %d\n", count_rra);
-	// printf("coun_rb: %d\n", count_rb);
-	// printf("coun_rrb: %d\n", count_rrb);
-	// printf("coun_rr: %d\n", count_rr);
-	// printf("coun_rrr: %d\n", count_rrr);
+	printf("coun_pa : %d\n", count_pa);
+	printf("coun_pb: %d\n", count_pb);
+	printf("coun_ra: %d\n", count_ra);
+	printf("coun_rra: %d\n", count_rra);
+	printf("coun_rb: %d\n", count_rb);
+	printf("coun_rrb: %d\n", count_rrb);
+	printf("coun_rr: %d\n", count_rr);
+	printf("coun_rrr: %d\n", count_rrr);
+	
 	printf("total hits: %d\n", *hits);
 
-	return (a);
-}
-
-t_stack	*sort_lg(t_stack *a, t_stack *b, int *hits)
-{
-	int			pivot;
-	int			count_pa;
-	int			count_pb;
-	int			count_ra;
-	int			count_rb;
-	int			count_rra;
-	int			count_rrb;
-
-	count_pa = 0;
-	count_pb = 0;
-	count_ra = 0;
-	count_rb = 0;
-	count_rra = 0;
-	count_rrb = 0;
-	pivot = get_pivot(a, b);
-	// printf("\n*** PARTITION BEGIN *****\n\n");
-	while (1)
-	{
-		if (a->top.value < pivot)
-		{
-			display_stacks(a, b);
-			*hits += push_stack(b, a, "pb\n");
-			count_pb++;
-		}
-		else if (is_rev_rotate(a, pivot))
-		{
-			*hits += rotate_stack(a, "rra\n");
-			count_rra++;
-		}
-		else
-		{
-			display_stacks(a, b);
-			*hits += rotate_stack(a, "ra\n");
-			count_ra++;
-		}
-		if (is_sorted(a, ASC) && a->top.value > b->max.value)
-			break ;
-		if (is_empty(a))
-			break ;
-		if (a->min.value == pivot)
-			pivot = get_pivot(a, b);
-	}
-	// printf("\n*** PARTITION END *****\n\n");
-	while (!is_empty(b))
-	{
-		if (b->max.index <= b->size / 2)
-		{
-			while (b->max.value != b->top.value)
-			{
-				*hits += rotate_stack(b, "rb\n");
-				count_rb++;
-			}
-		}
-		else
-		{
-			while (b->max.value != b->top.value)
-			{
-				*hits += rev_rotate_stack(b, "rrb\n");
-				count_rrb++;
-			}
-		}
-		*hits += push_stack(a, b, "pa\n");
-		count_pa++;
-	}
 	return (a);
 }
 
@@ -562,8 +599,6 @@ t_stack	*sort(t_stack *a, t_stack *b, int *hits)
 		return (sort_xs(a, hits));
 	if (a->size <= 5)
 		return (sort_sm(a, b, hits));
-	if (a->size <= 100)
-		return (sort_md(a, b, hits));
 	return (sort_md(a, b, hits));
 
 }
